9a47cc8a2bcd04f5f008245767ef5db14c71c600
sahil rana <74972430+sahil825@users.noreply.github.com>
2021-09-20 10:09:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,98 +1,143 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy 
-from flask_marshmallow import Marshmallow 
-import os
+from os import name
+from flask import Flask, Response, request
+import pymongo
+import json
+import bson.objectid
+from pymongo.message import delete 
 
-# Init app
+from werkzeug.wrappers import response
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init db
-db = SQLAlchemy(app)
-# Init ma
-ma = Marshmallow(app)
 
-# Product Class/Model
-class Product(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), unique=True)
-  description = db.Column(db.String(200))
-  price = db.Column(db.Float)
-  qty = db.Column(db.Integer)
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost", 
+        port =27017,
+        serverSelectionTimeoutMs = 1000
+        )
+        db = mongo.Company 
+        mongo.server_info() 
+except:
+    print("Error - Cannot connect to db")
 
-  def __init__(self, name, description, price, qty):
-    self.name = name
-    self.description = description
-    self.price = price
-    self.qty = qty
+#########################
+@app.route("/users", methods=["GET"])
+def get_some_users():
+    try:
+        data = list(db.users.find())
+        for user in data:
+            user ["_id"]= str(user["_id"])
+        return response(response = json.dumps([{"id":1},{"id":2}]),
+            status=500,
+            mimetype="application/json"
+            )
 
-# Product Schema
-class ProductSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'name', 'description', 'price', 'qty')
+    except Exception as ex:
+        print(ex)
+        return response(response = json.dumps({"massage":"cannot read user"}),
+            status=500,
+            mimetype="application/json"
+            )
 
-# Init schema
-product_schema = ProductSchema(strict=True)
-products_schema = ProductSchema(many=True, strict=True)
 
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
 
-  new_product = Product(name, description, price, qty)
+#########################
 
-  db.session.add(new_product)
-  db.session.commit()
+@app.root("/user", method=["POST"])
+def create_user():
+    try:
+        user = {
+            "name":request.form["name"], 
+            "LastName":request.form["last name"]
+            }
+        dbResponse = db.user.instert_one(user)
+        print(dbResponse.insterted_id)
+        #for attr in dir(dbResponse):
+           # print(attr)
+        return Response(
+            response= json.dumps(
+            response={"massage":"user created", 
+            "id":f"{dbResponse.insert_id}"
+            }),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as ex:
+        print("**************")
+        print(ex)
+        print("**************")
 
-  return product_schema.jsonify(new_product)
+#######################
+@app.route("/user/<id>", method=["PATCH"])
+def update_user(id):
+    
+    try:
+        dbResponse = db.user.update_one(
+            {"_id":bson.objectid.ObjectId(id)},
+            {"$set":{"name":request.form["name"]}}
+        )
+            if dbResponse.modified_count == 1:
+               return Response(
+            response= json.dumps(
+                {"massage":"user updated"}),
+            status=200,
+            mimetype="application/json"
+        ) 
+            else:
+                return Response(
+            response= json.dumps(
+                {"massage":"nothing to updated"}),
+            status=200,
+            mimetype="application/json"
 
-# Get All Products
-@app.route('/product', methods=['GET'])
-def get_products():
-  all_products = Product.query.all()
-  result = products_schema.dump(all_products)
-  return jsonify(result.data)
+    except Exception as ex:
+        print("***************************")
+        print(ex)
+        print("***************************")
+        return Response(
+            response= json.dumps(
+                {"massage":"sorry cannot update user"}),
+            status=500,
+            mimetype="application/json"
+        )
 
-# Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
 
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
 
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
+########################
+@app.route("/user/<id>", methods=["DELETE"])
+def delete_user(id):
+    try:
+        dbResponse = db.user.delete_one({"_id:Objectid(id"})
+        if dbResponse.deleted_count == 1:
 
-  product.name = name
-  product.description = description
-  product.price = price
-  product.qty = qty
+        return Response(
+            response= json.dumps(
+                {"massage":"user delete","id":f"{id}"}),
+            status=200,
+            mimetype="application/json"
+        )
+            
+        return Response(
+            response= json.dumps(
+                {"massage":"user not found",id":f"{id}"}),
+            status=200,
+            mimetype="application/json"
+        )
 
-  db.session.commit()
+    except Exception as ex:
+        print("********")
+        print(ex)
+        print("********")
+        return Response(
+            response= json.dumps(
+                {"massage":"sorry cannot delete user"}),
+            status=500,
+            mimetype="application/json"
+        )
 
-  return product_schema.jsonify(product)
 
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  db.session.delete(product)
-  db.session.commit()
 
-  return product_schema.jsonify(product)
 
-# Run Server
-if __name__ == '__main__':
-  app.run(debug=True)
+########################
+
+if __name__ == "__main__":
+    app.run(port=80, debug=True)
